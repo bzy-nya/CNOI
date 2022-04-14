5e9d9bc8abb122f3a661ca9d4ac6cafe87cc3d68
bzy <bzy.mic@qq.com>
2022-04-14 20:00:23 +0800

    CNOI!

--- CNOI/「CNOI2020」Cirno's Easy Round/T1 - Circle/circle.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main() {
-	int n, p, k; 
-	cin >> n >> p >> k;
-	cout << 1ll * p * k % n;
-	return 0;
-}
--- CNOI/「CNOI2021」Cirno's Easy Round II/C - Interval/SampleProgram.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-
-int Query(int x);
-
-void init() { }
-
-std::pair<int,int> Guess(int n, int c) {
-    return std::make_pair( 1, n );
-}
